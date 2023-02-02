1621c17a470ef829bfad93ac3ad9f5fcdaae1f45
Arthur Tatulyan <101785085+DonAbaddon@users.noreply.github.com>
2023-02-02 17:31:56 +0400

    Add files via upload

---
+++ Achievements/Achievements/Controllers/RatingRulesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Achievements.Controllers
+{
+    [ApiController]
+    [Route("ratingrules")]
+
+    public class RatingRulesController : ControllerBase
+    {
+        private static readonly int[] ratingCountingRules = { 0, 0, 0, 0, 0, 0 }; //по индексам: 0 - за невыполненную задачу, 1 - за выполненную, 2-5 - за соответсвующую оценку
+
+        public static int[] GetRatingRules()
+        {
+            return ratingCountingRules;
+        }
+
+        public static void SetRatingRules(int[] newRatingRules)
+        {
+            for(int i = 0; i < ratingCountingRules.Length; i++)
+            {
+                ratingCountingRules[i] = newRatingRules[i];
+            }
+        }
+
+        [HttpGet(Name = "Получить правила подсчета рейтинга")]
+        public int[] Get()
+        {
+            if (AuthenticationController.IsUserAuthorized())
+            {
+                return ratingCountingRules;
+            }
+            else return null;
+        }
+
+        [HttpPost(Name = "Изменить правило подсчета рейтинга")]
+        public void ChangeRule(int ruleIndex, int points)
+        {
+            if (AuthenticationController.IsAuthorizedAdult())
+            {
+                ratingCountingRules[ruleIndex] = points;
+            }
+        }
+    }
+}
---
+++ Achievements/Achievements/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿namespace Achievements.Models
+{
+    public class Rating
+    {
+        public int ID { get; set; }
+        public int ratingPoints { get; set; }
+
+        public Rating(int ID, int ratingPoints)
+        {
+            this.ID = ID;
+            this.ratingPoints = ratingPoints;
+        }
+    }
+}
